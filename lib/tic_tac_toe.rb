d6051e14dbd98914a58e4352c01254c7b5f62a25
Doak Covington <doakcovington@protonmail.com>
2020-02-05 03:46:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,16 @@
 class TicTacToe
 
+  WIN_COMBINATIONS = [
+    [0,1,2],#top row 0
+    [3,4,5],#middle row 1
+    [6,7,8],#bottom row 2
+    [0,3,6],#left column 3
+    [1,4,7],#middle column 4
+    [2,5,8],#right column 5
+    [0,4,8],#top left to bottom right 6
+    [2,4,6]#top right to bottom left 7
+  ]
+
   def initialize
     @board = Array.new(9, " ") # creates new array that contains 9 elements
   end
