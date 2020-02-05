a480195c71b6a4809cf8902b5d309420078dde44
Doak Covington <doakcovington@protonmail.com>
2020-02-05 03:44:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,7 @@
 class TicTacToe
 
   def initialize
-    @board = array.new(9, " ") # creates new array that contains 9 elements
+    @board = Array.new(9, " ") # creates new array that contains 9 elements
   end
 
 end
